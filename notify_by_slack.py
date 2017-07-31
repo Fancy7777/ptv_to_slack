44c48baa09c9da84f2b574fabd13601810a9121a
Fancy <deruiw@student.unimelb.edu.au>
2017-07-31 22:14:28 +1000

    Add files via upload
    update the notify_by_slack file. delete my certificate

--- notify_by_slack.py
+++ notify_by_slack.py
@@ -4,7 +4,9 @@ import urllib, urllib2
 #generate your own api token for later use
 def call_slack_api(user, message):
     slack_base = "https://slack.com/api/"
-    api_token = "chat.postMessage?token=xoxp-219773863122-220665630998-220670148582-d9471c6d4f68cdfd0cb6a68ccd32275d"
+
+    #add your own api-token here
+    api_token = "example"
     args = {"channel":"@"+user, "text":message}
     url = slack_base + api_token + "&" + urllib.urlencode(args)
     urllib2.urlopen(url)
